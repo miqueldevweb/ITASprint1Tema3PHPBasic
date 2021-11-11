4e7ca2c8aba2b613ef4904f3b3a6ddf1a7a3f44d
Miquel <miqueldevweb@gmail.com>
2021-11-11 12:07:31 +0100

    Fi exercici4. Inici exercici5

---
+++ Nivell1/exercici4.php
@@ -0,0 +1,28 @@
+<?php
+$X = 14;
+$Y = 25;
+$N = 3.2154;
+$M = 6.21354;
+
+echo "x = " . $X . "<br>";
+echo "y = " . $Y . "<br>";
+echo "x + y = " . $X + $Y . "<br>";
+echo "x - y = " . $X - $Y . "<br>";
+echo "x * y = " . $X * $Y . "<br>";
+echo "x % y = " . $X % $Y . "<br><br>";
+
+echo "n = " . $N . "<br>";
+echo "m = " . $M . "<br>";
+echo "n + m = " . $N + $M . "<br>";
+echo "n - m = " . $N - $M . "<br>";
+echo "n * m = " . $N * $M . "<br>";
+echo "n % m = " . $N % $M . "<br><br>";
+
+echo "2 * x = " . 2 * $X . "<br>";
+echo "2 * y = " . 2 * $Y . "<br>";
+echo "2 * n = " . 2 * $N . "<br>";
+echo "2 * m = " . 2 * $M . "<br><br>";
+
+echo "x + y + n + m = " . $X + $Y + $N + $M . "<br>";
+echo "x * y * n * m = " . $X * $Y * $N * $M . "<br>";
+?>
