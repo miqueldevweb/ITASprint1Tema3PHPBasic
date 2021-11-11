3acec7c99268d41324c0a397247df9d56588635b
Miquel <miqueldevweb@gmail.com>
2021-11-11 11:31:01 +0100

    Finalitza exercici2, es corregeix directori. Inicia exercici3

---
+++ Nivell1/exercici2.php
@@ -0,0 +1,10 @@
+<?php
+//Variables
+$saluda = "Hello, World!";
+$curs = "Aquest és el curs de PHP";
+
+//Mètodes
+echo strlen($saluda) . "<br>";
+echo strrev($saluda) . "<br>";
+echo $saluda . $curs;
+?>
