cad71886d6a8619b594592cb7defcd97040974da
Miquel <miqueldevweb@gmail.com>
2021-11-13 08:25:24 +0100

    Correccio de l'exercici2 Nivell1

--- Nivell1/exercici2.php
+++ Nivell1/exercici2.php
@@ -4,6 +4,7 @@ $saluda = "Hello, World!";
 $curs = "Aquest és el curs de PHP";
 
 //Mètodes
+echo strtoupper($saluda) . "<br>";
 echo strlen($saluda) . "<br>";
 echo strrev($saluda) . "<br>";
 echo $saluda . $curs;
